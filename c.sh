196e17e3d493d22bc30f239029fe93f59eadd9c7
2573874409@qq.com <z18344203426@qq.com>
2023-01-11 11:29:58 +0800

    [调试]: FIX

--- c.sh
+++ c.sh
@@ -1,15 +1,15 @@
 wget --no-dns-cache --random-wait --continue -d -T 60 -t 30 "$dLINK/$dNAMR"
- unzip -d './tmp' "./$dNAME"
- cd './tmp'
- cat './pack.mcmeta' | jq -M "."pack"."pack_format"=$cV" > './tmp.txt'
- cp './tmp.txt' './pack.mcmeta'
- rm './tmp.txt'
- zip "./$cNAME" *
- md5sum "./$cNAME" "/$mNAME"
- sed 's/  //' > "/$mNAME"
- sed "s/$cNAME//" > "/$mNAME"
- mv ./$cNAME ../
- mv "./$mNAME" ../
- cd ../
- rm -r './tmp'
- mv "./$cNAME" './files'
+unzip -d 'tmp' "$dNAME"
+cd 'tmp'
+cat 'pack.mcmeta' | jq -M "."pack"."pack_format"=$cV" > 'tmp.txt'
+cp 'tmp.txt' 'pack.mcmeta'
+rm 'tmp.txt'
+zip "$cNAME" *
+md5sum "$cNAME" "$mNAME"
+sed 's/  //' > "$mNAME"
+sed "s/$cNAME//" > "$mNAME"
+mv "$cNAME" ../
+mv "$mNAME" ../
+cd ../
+rm -r 'tmp'
+mv "$cNAME" 'files'
