fa8b58c4eba247c0577e130c67fe2f60513312dc
2573874409@qq.com <z18344203426@qq.com>
2023-01-11 12:52:08 +0800

    [调试]：

--- c.sh
+++ c.sh
@@ -1,15 +1,17 @@
 wget --no-dns-cache --random-wait --continue -d -T 60 -t 30 "$dLINK/$dNAMR"
 unzip -d 'tmp' "$dNAME"
-cd 'tmp'
-cat 'pack.mcmeta' | jq -M "."pack"."pack_format"=$cV" > 'tmp.txt'
-cp 'tmp.txt' 'pack.mcmeta'
-rm 'tmp.txt'
+cd './tmp'
+cat './pack.mcmeta' | jq -M "."pack"."pack_format"=$cV" > './tmp.txt'
+cp './tmp.txt' './pack.mcmeta'
+rm './tmp.txt'
 zip "$cNAME" *
-md5sum "$cNAME" "$mNAME"
-sed 's/  //' > "$mNAME"
-sed "s/$cNAME//" > "$mNAME"
-mv "$cNAME" ../
-mv "$mNAME" ../
+md5sum "./$cNAME" > "./$mNAME"
+sed 's/  //' > "./$mNAME"
+sed "s/$cNAME//" > "./$mNAME"
+mv "./$cNAME" ../
+mv "./$mNAME" ../
 cd ../
-rm -r 'tmp'
-mv "$cNAME" 'files'
+rm -r './tmp'
+mv "./$cNAME" 'files'
+mv "./$mNAME" 'files'
+
